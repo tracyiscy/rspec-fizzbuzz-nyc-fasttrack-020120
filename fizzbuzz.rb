dce36e589e17f2895d74ab843cd58a86bc8da93f
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-02-01 18:44:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if (int % 3 == 0) & (int % 5 == 0)
-    return "Fizz"
+    return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
   elsif int % 3 == 0
-    return "FizzBuzz"
+    return "Fizz"
     end
 end
